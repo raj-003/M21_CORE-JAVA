c03f2e5927be088a31ff7596d3d8d6310e6e27aa
Vishwanath <Vishwanath@MSI>
2022-07-12 14:40:23 +0530

    fifth commit

---
+++ Day-2-intro-to-java/src/com/cg/java/pgm2.java
@@ -0,0 +1,18 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class pgm2 {
+
+	public static void main(String[] args) {
+		System.out.println("enter your string");
+			Scanner s=new Scanner(System.in);
+			char a;
+			String String=s.next();
+			System.out.println("Your String is :" +String);
+			//String String=s.next();
+			s.close();		
+			
+	}
+
+}
