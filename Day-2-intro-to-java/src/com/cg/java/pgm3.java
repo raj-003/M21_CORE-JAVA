02ceabbc1e1476ac9bb812ee0b567e2c2b5e2e3f
Vishwanath <Vishwanath@MSI>
2022-07-12 14:52:29 +0530

    sixth commit

---
+++ Day-2-intro-to-java/src/com/cg/java/pgm3.java
@@ -0,0 +1,17 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class pgm3 {
+
+	public static void main(String[] args) {
+		System.out.println("enter your number");
+			Scanner s=new Scanner(System.in);
+			float a1=s.nextFloat();
+			float a;
+			System.out.println("your number is :" +a1);
+		
+			//System.out.println("enter your number");
+	}
+
+}
