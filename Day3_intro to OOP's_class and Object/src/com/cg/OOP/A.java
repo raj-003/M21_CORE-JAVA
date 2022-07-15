49ca69e03043a3bc5c7965ca288300cd08531a80
Vishwanath <Vishwanath@MSI>
2022-07-15 21:34:03 +0530

    Eigth commit

---
+++ Day3_intro to OOP's_class and Object/src/com/cg/OOP/A.java
@@ -0,0 +1,19 @@
+package com.cg.OOP;
+
+public class A { 
+	int num;
+    String name;
+    A()
+    
+    	{ System.out.println("Constructor called"); }
+    }
+
+/*class B{
+    public static void main(String[] args)
+    {
+        A a = new A();
+
+        System.out.println(a.name);
+        System.out.println(a.num);
+    }
+}*/
--- Day4_Encapsulation_abstraction_Packages/src/com/cg/abstr/A.java
+++
@@ -1,9 +0,0 @@
-package com.cg.abstr;
-
-public class A {
-public void print() {
-	String str= "Hello";
-	System.out.print("hey there "+str);
-}
-
-}
