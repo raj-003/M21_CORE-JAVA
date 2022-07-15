e7e91fc22808366ea4fdbaa6782f303a25b0f995
Vishwanath <Vishwanath@MSI>
2022-07-15 21:47:30 +0530

    nineth commit

--- Day-2-intro-to-java/src/com/cg/java/pgm1.java
+++ Day-2-intro-to-java/src/com/cg/java/pgm1.java
@@ -7,9 +7,9 @@ public class pgm1 {
 	public static void main(String[] args) {
 		System.out.println("enter your number");
 			Scanner s=new Scanner(System.in);
-			int a;
-			int a1=s.nextInt();
-		
+			Float a;
+			Float a1=s.nextFloat();
+			System.out.println("your number is:"+a1);
 			
 	}
 
