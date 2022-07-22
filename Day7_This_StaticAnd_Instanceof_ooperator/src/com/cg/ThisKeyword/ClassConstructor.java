01c6d63ad29f6150d16a48b82280780fc55e9d2e
Vishwanath <Vishwanath@MSI>
2022-07-22 12:17:01 +0530

    first commit

--- Day7_This_StaticAnd_Instanceof_ooperator/src/com/cg/ThisKeyword/ClassConstructor.java
+++ Day7_This_StaticAnd_Instanceof_ooperator/src/com/cg/ThisKeyword/ClassConstructor.java
@@ -3,17 +3,21 @@ package com.cg.ThisKeyword;
 public class ClassConstructor {
 	int a=10;
  ClassConstructor(){
-		this(a);
+	
 		System.out.println("Inside default constructor");
 	}
-	void ClassConstructor(int a) {
+	ClassConstructor(int a) {
 		this.a=a;
 		System.out.println("Inside Parameter constructor");
 	}
 
 	public static void main(String[] args) {
-		ClassConstructor a= new ClassConstructor();
-	a.ClassConstructor(10);
+		ClassConstructor c= new ClassConstructor();
+		c.ClassConstructor();
+	}
+	
+		
+		
 	}
 
-}
+
