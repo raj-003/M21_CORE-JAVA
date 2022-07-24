6846bb43f13f0c6acb05e60d041492e629bccfe3
Vishwanath <Vishwanath@MSI>
2022-07-24 16:25:00 +0530

    First commit

---
+++ Day11_Strings/src/com/cg/Strings/Example1.java
@@ -0,0 +1,11 @@
+package com.cg.Strings;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		String str="hello";
+		System.out.println(str);
+
+	}
+
+}
---
+++ Day11_Strings/src/com/cg/Strings/Length.java
@@ -0,0 +1,15 @@
+package com.cg.Strings;
+
+import java.util.Scanner;
+
+public class Length {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		String str=s.nextLine();
+		System.out.println(str.length());
+		System.out.println(str.charAt(2));
+		s.close();
+	}
+
+}
