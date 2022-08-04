1fde2ceea58d9bf37b4a8cc18ad109f5cb35ec54
Vishwanath <Vishwanath@MSI>
2022-08-04 08:53:24 +0530

    First commit

---
+++ Day20_Annotations/src/com/cg/annotations/SuppressWarningAnnotations.java
@@ -0,0 +1,15 @@
+package com.cg.annotations;
+class Animal{
+	Animal(){
+		System.out.println("Lion Roars");
+	}
+}
+
+public class SuppressWarningAnnotations {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Animal a= new Animal();
+	}
+
+}
