9a69ac649b9ff00c8ea439af04921fd03b5d1315
Vishwanath <Vishwanath@MSI>
2022-08-09 20:38:33 +0530

    Third commit

--- Day26-Banking Application/src/com/cg/client/Client.java
+++ Day26-Banking Application/src/com/cg/client/Client.java
@@ -11,11 +11,16 @@ public class Client {
 
 	public static void main(String[] args) {
 		BankFactory b = new MMBankFactory();
-		SavingAcc s = new MMSavingAcc(123," Raj ",1000000,true);
+		SavingAcc s = new MMSavingAcc(123," Raj ",500000,true);
 		CurrentAcc c = new MMCurrentAcc(345," RAJ ",100000,50000);
+		
 		System.out.println("Saving Account");
-		s.withdraw(1000);
 		System.out.println(s);
+		System.out.println("Balance of Saving Account is:"+s.getAccBal());
+		s.withdraw(1000);
+		System.out.println("Balance after withdrawl amount is :"+s.getAccBal());
+	
+		
 		System.out.println();
 		System.out.println("Current Account");
 		System.out.println("Balance of Current Account is:"+c.getAccBal());
